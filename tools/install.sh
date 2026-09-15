e968558af7b3a106383845259a0f770f632a8819
liufang <liufang_robot@outlook.com>
2026-09-15 14:13:01 +0800

    fix: enable declared OS dependencies during installation

--- tools/install.sh
+++ tools/install.sh
@@ -74,7 +74,11 @@ orocos_rock_require_autoproj
 orocos_rock_ensure_workspace_ruby_gems
 orocos_rock_source_workspace_env
 orocos_rock_configure_target_environment "$TARGET"
-orocos_rock_prepare_autoproj_workspace "$PREFIX" "none" "$TARGET"
+if [ "$INSTALL_OSDEPS" -eq 1 ]; then
+    orocos_rock_prepare_autoproj_workspace "$PREFIX" "all" "$TARGET"
+else
+    orocos_rock_prepare_autoproj_workspace "$PREFIX" "none" "$TARGET"
+fi
 
 cd "$OROCOS_ROCK_ROOT"
 
